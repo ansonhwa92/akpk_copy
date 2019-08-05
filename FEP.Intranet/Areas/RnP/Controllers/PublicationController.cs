c1a31defd5fa59b16bcf14ea14d0aa79e775eefb
lesmiettes2014 <kimabajon18@gmail.com>
2019-08-05 18:51:37 +0800

    4th commit

---
+++ FEP.Intranet/Areas/RnP/Controllers/PublicationController.cs
@@ -0,0 +1,18 @@
+﻿using FEP.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FEP.Intranet.Areas.RnP.Controllers
+{
+    public class PublicationController : FEPController
+    {
+        // GET: RnP/Publication
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FEP.Intranet/Areas/RnP/RnPAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace FEP.Intranet.Areas.RnP
+{
+    public class RnPAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "RnP";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "RnP_default",
+                "RnP/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

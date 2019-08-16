dbe3f4da6f1c359658b4a882d1aa8a668b4c4aec
lesmiettes2014 <kimabajon18@gmail.com>
2019-08-16 10:38:04 +0800

    firus_160819

---
+++ FEP.WebApi/Api/RnP/PublicationController.cs
@@ -0,0 +1,173 @@
+﻿using FEP.Model;
+using FEP.WebApiModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace FEP.WebApi.Api.RnP
+{
+    [Route("api/RnP/Publication")]
+    public class PublicationController : ApiController
+    {
+        private DbEntities db = new DbEntities();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // GET: api/RnP/Publication (all)
+        public List<PublicationApiModel> Get()
+        {
+            var publications = db.Publication.OrderBy(p => p.Title).Select(s => new PublicationApiModel
+            {
+                ID = s.ID,
+                CategoryID = s.CategoryID,
+                Author = s.Author,
+                Coauthor = s.Coauthor,
+                Title = s.Title,
+                Year = s.Year,
+                Description = s.Description,
+                Language = s.Language,
+                ISBN = s.ISBN,
+                Free = s.Free,
+                Hardcopy = s.Hardcopy,
+                Digitalcopy = s.Digitalcopy,
+                HDcopy = s.HDcopy,
+                HPrice = s.HPrice,
+                DPrice = s.DPrice,
+                HDPrice = s.HDPrice,
+                ProofOfApproval = s.ProofOfApproval,
+                StockBalance = s.StockBalance,
+                WithdrawalReason = s.WithdrawalReason,
+                ProofOfWithdrawal = s.ProofOfWithdrawal,
+                DateAdded = s.DateAdded,
+                Status = s.Status,
+                WStatus = s.WStatus,
+                ViewCount = s.ViewCount,
+                PurchaseCount = s.PurchaseCount
+            }).ToList();
+
+            return publications;
+        }
+
+        // GET: api/RnP/Publication/{status} (published/etc.)
+        public List<PublicationApiModel> Get(PublicationStatus status)
+        {
+            var publications = db.Publication.Where(p => p.Status == status).OrderBy(p => p.Title).Select(s => new PublicationApiModel
+            {
+                ID = s.ID,
+                CategoryID = s.CategoryID,
+                Author = s.Author,
+                Coauthor = s.Coauthor,
+                Title = s.Title,
+                Year = s.Year,
+                Description = s.Description,
+                Language = s.Language,
+                ISBN = s.ISBN,
+                Free = s.Free,
+                Hardcopy = s.Hardcopy,
+                Digitalcopy = s.Digitalcopy,
+                HDcopy = s.HDcopy,
+                HPrice = s.HPrice,
+                DPrice = s.DPrice,
+                HDPrice = s.HDPrice,
+                ProofOfApproval = s.ProofOfApproval,
+                StockBalance = s.StockBalance,
+                WithdrawalReason = s.WithdrawalReason,
+                ProofOfWithdrawal = s.ProofOfWithdrawal,
+                DateAdded = s.DateAdded,
+                Status = s.Status,
+                WStatus = s.WStatus,
+                ViewCount = s.ViewCount,
+                PurchaseCount = s.PurchaseCount
+            }).ToList();
+
+            return publications;
+        }
+
+        // GET: api/RnP/Publication/5
+        public PublicationApiModel Get(int id)
+        {
+            var publication = db.Publication.Where(p => p.ID == id).Select(s => new PublicationApiModel
+            {
+                ID = s.ID,
+                CategoryID = s.CategoryID,
+                Author = s.Author,
+                Coauthor = s.Coauthor,
+                Title = s.Title,
+                Year = s.Year,
+                Description = s.Description,
+                Language = s.Language,
+                ISBN = s.ISBN,
+                Free = s.Free,
+                Hardcopy = s.Hardcopy,
+                Digitalcopy = s.Digitalcopy,
+                HDcopy = s.HDcopy,
+                HPrice = s.HPrice,
+                DPrice = s.DPrice,
+                HDPrice = s.HDPrice,
+                ProofOfApproval = s.ProofOfApproval,
+                StockBalance = s.StockBalance,
+                WithdrawalReason = s.WithdrawalReason,
+                ProofOfWithdrawal = s.ProofOfWithdrawal,
+                DateAdded = s.DateAdded,
+                Status = s.Status,
+                WStatus = s.WStatus,
+                ViewCount = s.ViewCount,
+                PurchaseCount = s.PurchaseCount
+            }).FirstOrDefault();
+
+            //approvals & withdrawal approvals
+            var approvals = db.PublicationApproval.Where(pa => pa.PublicationID == publication.ID).ToList();
+            var withdrawals = db.PublicationWithdrawal.Where(pw => pw.PublicationID == publication.ID).ToList();
+
+            publication.Approvals = approvals;
+            publication.Withdrawals = withdrawals;
+
+            return publication;
+        }
+
+        // POST: api/RnP/Publication
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, new { isSuccess = true });
+            return response;
+        }
+
+        // PUT: api/RnP/Publication/5
+        public HttpResponseMessage Put(int id, [FromBody]string value)
+        {
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, new { isSuccess = true });
+            return response;
+        }
+
+        // DELETE: api/RnP/Publication/5
+        public bool Delete(int id)
+        {
+            var publication = db.Publication.Where(p => p.ID == id).FirstOrDefault();
+
+            if (publication != null)
+            {
+                db.Publication.Remove(publication);
+                //db.Configuration.ValidateOnSaveEnabled = false;
+                db.SaveChanges();
+
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+}
---
+++ FEP.WebApiModel/RnP/Publication.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FEP.Model;
+
+namespace FEP.WebApiModel
+{
+    public class PublicationApiModel
+    {
+        public int ID { get; set; }
+        public int CategoryID { get; set; }
+        public string Author { get; set; }
+        public string Coauthor { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Description { get; set; }
+        public string Language { get; set; }
+        public string ISBN { get; set; }
+        public bool Free { get; set; }
+        public bool Hardcopy { get; set; }
+        public bool Digitalcopy { get; set; }
+        public bool HDcopy { get; set; }
+        public float HPrice { get; set; }
+        public float DPrice { get; set; }
+        public float HDPrice { get; set; }
+        public string ProofOfApproval { get; set; }
+        public int StockBalance { get; set; }
+        public string WithdrawalReason { get; set; }
+        public string ProofOfWithdrawal { get; set; }
+        public DateTime? DateAdded { get; set; }
+        public PublicationStatus Status { get; set; }
+        public PublicationWithdrawalStatus WStatus { get; set; }
+        public int ViewCount { get; set; }
+        public int PurchaseCount { get; set; }
+        public List<PublicationApproval> Approvals { get; set; }
+        public List<PublicationWithdrawal> Withdrawals { get; set; }
+    }
+
+    public class PublicationCategoryApiModel
+    {
+
+    }
+}

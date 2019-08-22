c4ca71f3b9b255e4413b314557c9f633d5e0251b
lesmiettes2014 <kimabajon18@gmail.com>
2019-08-22 16:10:10 +0800

    220819_3

--- FEP.Model/Migrations/Configuration.cs
+++ FEP.Model/Migrations/Configuration.cs
@@ -5,18 +5,19 @@ namespace FEP.Model.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<DbEntities>
+    internal sealed class Configuration : DbMigrationsConfiguration<FEP.Model.DbEntities>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(DbEntities context)
+        protected override void Seed(FEP.Model.DbEntities context)
         {
-            mhafeez.Seed(context);
-            aiman.Seed(context);
-            firus.Seed(context);
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
         }
     }
 }

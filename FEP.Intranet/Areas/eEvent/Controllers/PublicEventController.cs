6dd271b4a0ff73193f4d652839a9e40fd136a7bf
muhammad aiman bin nor amri <aimannoramri@gmail.com>
2019-08-24 04:45:58 +0800

    merge dgn firus

--- FEP.Intranet/Areas/eEvent/Controllers/PublicEventController.cs
+++ FEP.Intranet/Areas/eEvent/Controllers/PublicEventController.cs
@@ -2,11 +2,9 @@
 using FEP.Intranet.Areas.eEvent.Models;
 using FEP.Model;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 
 namespace FEP.Intranet.Areas.eEvent.Controllers

a65dc981c55f8fb2a6995c6eb153f908bb2c621b
Hasfaizura <hasfaizura@primuscore.com>
2019-11-18 15:39:17 +0800

    Create course - Fix Review before submission

--- FEP.Intranet/Areas/eLearning/Controllers/CoursesController.cs
+++ FEP.Intranet/Areas/eLearning/Controllers/CoursesController.cs
@@ -208,10 +208,10 @@ namespace FEP.Intranet.Areas.eLearning.Controllers
 
             var model = _mapper.Map<CreateOrEditCourseModel>(course);
 
-            model.Description = HttpUtility.HtmlDecode(model.Description);
-            model.Objectives = HttpUtility.HtmlDecode(model.Objectives);
+            //model.Description = HttpUtility.HtmlDecode(model.Description);
+            //model.Objectives = HttpUtility.HtmlDecode(model.Objectives);
 
-            ViewBag.CategoryId = new SelectList(db.PublicationCategory, "Id", "Name", course.CategoryId);
+            ViewBag.CategoryId = new SelectList(db.RefCourseCategories, "Id", "Name", course.CategoryId);
 
             return View(model);
         }
